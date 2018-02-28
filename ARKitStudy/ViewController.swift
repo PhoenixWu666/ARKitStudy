f9fcc9554978d7a60dc049e04fa69e0c04aa6b1f
Phoenix Wu <phoenix@funpodium.net>
2018-02-28 20:42:06 +0800

    Initial Commit

---
+++ ARKitStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitStudy
+//
+//  Created by Phoenix Wu on H30/02/28.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
